a0ce985dc0ed50d75c1b211e49a7f0eeba45f6a9
lsj <jungy0309@naver.com>
2023-02-08 19:31:45 +0900

    2023/02/08

---
+++ kmarket/src/main/java/kr/co/kmarket/repository/ProductRepo.java
@@ -0,0 +1,6 @@
+package kr.co.kmarket.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepo extends JpaRepository<ProductRepo, Integer> {
+}
---
+++ kmarket/src/main/java/kr/co/kmarket/service/ProductService.java
@@ -0,0 +1,30 @@
+package kr.co.kmarket.service;
+
+import kr.co.kmarket.dao.ProductDAO;
+import kr.co.kmarket.repository.ProductRepo;
+import kr.co.kmarket.vo.ProductVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductDAO dao;
+
+    @Autowired
+    private ProductRepo repo;
+
+    public void insertProduct(ProductVO vo){
+        dao.insertProduct(vo);
+    }
+
+    public void selectProduct () {}
+
+    public List<ProductVO> selectProducts(int start) {
+        return dao.selectProducts(start);
+    }
+
+}
