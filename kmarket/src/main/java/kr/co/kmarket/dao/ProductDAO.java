02432b624ec43fa3aa075b38e4ba0576bcdf6a68
lsj <jungy0309@naver.com>
2023-02-09 19:39:53 +0900

    2023/02/09

--- kmarket/src/main/java/kr/co/kmarket/dao/ProductDAO.java
+++ kmarket/src/main/java/kr/co/kmarket/dao/ProductDAO.java
@@ -5,12 +5,15 @@ import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+/**
+ * @since 2023/02/08 라성준
+ */
 @Mapper
 @Repository
 public interface ProductDAO {
     public void insertProduct(ProductVO vo);
-//    public int updateProduct(ProductVO vo);
-//    public int deleteProduct(int no);
-//    public List<ProductVO> selectProducts(int start);
+    public List<ProductVO> selectProducts(int start);
+    //public int selectCountTotalProduct() {}
+    public int updateProduct(ProductVO vo);
+    public int deleteProduct(int no);
 }
--- kmarket/src/main/java/kr/co/kmarket/service/ProductService.java
+++ kmarket/src/main/java/kr/co/kmarket/service/ProductService.java
@@ -7,22 +7,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+/**
+ * @since 2023/02/08 라성준
+ * @param
+ * @return
+ */
 @Service
 public class ProductService {
 
-//    @Autowired
-//    private ProductDAO dao;
-//
-//    @Autowired
-//    private ProductRepo repo;
+   @Autowired
+   private ProductDAO dao;
 
-//    public void insertProduct(ProductVO vo){ dao.insertProduct(vo); }
-//
-//    public void selectProduct () {}
-//
-//    public List<ProductVO> selectProducts(int start) {
-//        return dao.selectProducts(start);
-//    }
+    @Autowired
+    private ProductRepo repo;
+
+    public void insertProduct(ProductVO vo){
+        dao.insertProduct(vo);
+    }
+
+    public void selectProduct () {}
+
+    public List<ProductVO> selectProducts(int start) {
+        return dao.selectProducts(start);
+    }
+
+    //public int selectCountTotalProduct() {
+    //  int total = dao.selectCountTotalProduct();
+    //   return total;
+    //  }
+
+    public int deleteProduct (int prodNo) {
+        return dao.deleteProduct(prodNo);
+    }
+
+    public int updateProduct(ProductVO vo) {
+        return dao.updateProduct(vo);
+    }
+
+
 
 }
